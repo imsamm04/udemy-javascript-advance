fcea12ea6398d8cdd26f2182a8b082ef175b1491
viet.dinhduc <ducviet2504@gmail.com>
2022-07-14 21:02:37 +0700

    update

--- 08-05-object.js
+++ 08-05-object.js
@@ -9,15 +9,31 @@
 // get average mark of an object
 // calcAvgMark ({math: 10, enghlish : 8}) -> 9
 
+// function calcAvgMark (mark) {
+//     if(!mark) return -1;
+//     const length = Object.keys(mark).length;
+//     let sum = 0;
+
+//     for(const key in mark) {
+//         const value = mark[key];
+//         sum += value;
+//     }
+//     return (sum / length).toFixed(1);
+// }
+// console.log(calcAvgMark({math: 5.9, english: 6}));
+
 function calcAvgMark (mark) {
+    if(!mark) return -1;
     const length = Object.keys(mark).length;
     let sum = 0;
-
     for(const key in mark) {
+       
         const value = mark[key];
         sum += value;
     }
-    return sum / length;
+    return (sum/length).toFixed(1)
+     // let sum = 10;
 }
-// console.log(length);
-console.log(calcAvgMark({math: 5, english: 6}));
+// let sum = 8;
+// console.log(sum);
+console.log(calcAvgMark({math: 4, english : 7}));

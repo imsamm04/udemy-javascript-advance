6137c594023c7a9a2318cb643e8dfb8dcff2dfbf
viet.dinhduc <ducviet2504@gmail.com>
2022-07-14 22:09:08 +0700

    update code

--- array-playround.js
+++ array-playround.js
@@ -3,9 +3,40 @@
 
 // console.log(anotherList[3]);
 
-// const numberList = [2, 4, 6];
+// const numberList = [2, 4, 6, 7];
 
 // for(let i =0;i< numberList.length;i++) {
 //     const number = numberList[i];
 //     console.log(number);
 // }
+
+//forech
+
+// numberList.forEach(x => {
+//     console.log(x);
+// });
+
+//for of (es6)
+
+// for (const number of numberList) {
+//     console.log(number);
+// }
+
+// function log() {
+//     console.log('hello');
+// }
+
+// const log = () => {
+//     console.log('hello');
+// }
+
+// numberList.forEach((number, index)=> {
+//     console.log(number, index);
+// }) 
+
+//add new item to the beginning
+
+const numberList = [1, 2, 3];
+ numberList.unshift(0);
+
+ console.log(numberList);

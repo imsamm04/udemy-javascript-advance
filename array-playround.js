ccccb1d8e025812a7d1e7bb884f4d09b82d53e6a
viet.dinhduc <ducviet2504@gmail.com>
2022-07-14 21:37:13 +0700

    update code

---
+++ array-playround.js
@@ -0,0 +1,11 @@
+// const numberList = [1, 2, 3];
+// const anotherList = [...numberList, 6];
+
+// console.log(anotherList[3]);
+
+const numberList = [2, 4, 6];
+
+for(let i =0;i< numberList.length;i++) {
+    const number = numberList[i];
+    console.log(number);
+}

7f61309085af907d515a6ced6e7d8a88c4619318
viet.dinhduc <ducviet2504@gmail.com>
2022-07-18 21:55:05 +0700

    ax

---
+++ array-main.js
@@ -0,0 +1,21 @@
+// const numberList = [1, 2, 3];
+// // numberList.push(4, 5)
+// // console.log(numberList);
+
+// // const lastNumber = numberList.pop();
+// // console.log(lastNumber);
+
+// const firstNumber = numberList.unshift(0);
+// console.log(numberList);
+
+// Add new items at the end
+const numberList = [1, 2, 3];
+// numberList.push(4, 5);
+// console.log(numberList); // [1, 2, 3, 4, 5]
+
+// const lastNumber = numberList.pop();
+// console.log(numberList, lastNumber);
+
+numberList.unshift(0);
+
+console.log(numberList)

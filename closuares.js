ea84b1d5a4b690baebf5a23f1b8806c59751a742
viet.dinhduc <ducviet2504@gmail.com>
2022-07-20 23:51:29 +0700

    update code

--- closuares.js
+++ closuares.js
@@ -1,35 +1,5 @@
-// function init() {
-//     var name = 'abc'
-//     function displayName() {
-//         console.log(name);
-//     }
-//     displayName();
-// }
-
-// init();
-
-
-
-// function init() {
-//     var name = "abc";
-//     function displayName() {
-//         console.log(name);
-//     }
-//     return displayName;
-// }
-
-// const myFunction = init();
-// myFunction();
-
-
-
-function makeFunction() {
-    var name = 'abc';
-    function displayName() {
-        console.log(name);
-    }
-    return displayName;
+for( var i = 0; i < 3 ; i++ ) {
+    setTimeout(function(){
+        console.log(i);
+    }, 1000 + i)
 }
-
-var myFunction = makeFunction();
-myFunction();

73ca12e90f5f00edb2ab9d1afde6ed0f992b4cd6
viet.dinhduc <ducviet2504@gmail.com>
2022-07-23 22:25:08 +0700

    update code

--- closuares.js
+++ closuares.js
@@ -5,12 +5,31 @@
 // console.log(num);
 
 // num = 6;
-console.log(sum(3, 4));
-function sum(a, b) {
-    var c = a + b
-    return c;
-}
+// console.log(sum(3, 4));
+// function sum(a, b) {
+//     var c = a + b
+//     return c;
+// }
 
 // console.log(c);
 
+// console.log(sum(2,3));
+// var sum = function (a, b) {
+//     return a + b;
+// }
+
+// sum(2, 3);
+
+// function sum(...numberList) {
+//     return numberList.reduce((total, number) => total + number, 6)
+// }
+
+// const numberList = [2, 4, 6];
+// console.log(sum(...numberList));
+
+// function sum () {} // function declaration
+
+// const sum = function() {} // function expression
+
+const sum = () => {} // arrow function
 

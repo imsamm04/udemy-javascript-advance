373ae3a115a5d1d14b50dcf414fda5036f85473a
viet.dinhduc <ducviet2504@gmail.com>
2022-07-25 23:11:56 +0700

    update code

--- closuares.js
+++ closuares.js
@@ -1,5 +1,8 @@
-function getAge(...args) {
-    console.log(typeof args);
-  }
-  
-  getAge(21);
+const numberList = [2, 4, 6];
+let sum = 0;
+for (let i = 0; i < numberList.length; i++) {
+ sum += numberList[i];
+}
+console.log(sum); // 12
+// the same with above but using reduce()
+numberList.reduce((sum, number) => sum + number); 

3a1dd350128443d9b4da565314864dbf1726e945
viet.dinhduc <ducviet2504@gmail.com>
2022-07-25 23:18:44 +0700

    update code

--- closuares.js
+++ closuares.js
@@ -1,8 +1,21 @@
-const numberList = [2, 4, 6];
-let sum = 0;
-for (let i = 0; i < numberList.length; i++) {
- sum += numberList[i];
+// const numberList = [2, 4, 6];
+// let sum = 0;
+// for (let i = 0; i < numberList.length; i++) {
+//  sum += numberList[i];
+// }
+// console.log(sum); // 12
+// // the same with above but using reduce()
+// numberList.reduce((sum, number) => sum + number); 
+
+function findMax(numberList) {
+  if(!Array.isArray(numberList) || numberList.length === 0) return undefined;
+  let max = numberList[0];
+  for(let i = 0; i < numberList.length; i++){
+    if(numberList[i] > max) {
+      max = numberList[i];
+    }
+  }
+return max;
 }
-console.log(sum); // 12
-// the same with above but using reduce()
-numberList.reduce((sum, number) => sum + number); 
+
+console.log(findMax([2,3,9,4,5]));

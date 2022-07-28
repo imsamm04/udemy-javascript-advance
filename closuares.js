aa033682975acfef4028980c263a268492f71930
viet.dinhduc <ducviet2504@gmail.com>
2022-07-28 23:14:04 +0700

    update code

--- closuares.js
+++ closuares.js
@@ -25,8 +25,26 @@
 // const arr = numberList.sort(); // [1, 2, 3, 5]
 // console.log(arr);
 
-const numbers = [4, 2, 100, 5, 1, 3];
-numbers.sort(function(a, b) {
-  return a - b;
-});
-console.log(numbers);
+// const numberList = [2, 4, 6];
+// // numbers.sort(function(a, b) {
+// //   return a - b;
+// // });
+// // console.log(numbers);
+// let sum = 0;
+// const totalNumber = numberList.reduce((sum, number)=> sum + number)
+
+// console.log(totalNumber);
+
+// const n = 0;
+// const n = 2;
+
+const numberList = [2, 4, 6];
+let sum = 0;
+// for (let i = 0; i < numberList.length; i++) {
+//  sum += numberList[i];
+// }
+// console.log(sum); // 12
+
+const equal = numberList.reduce((sum, number) => sum + number); 
+
+console.log(equal); // 12
